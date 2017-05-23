7fd1de69077d262962ad856cf6381b8897a761c0
1638h55 <syqvb@126.com>
2017-05-23 20:37:11 +0800

    mine

---
+++ js/loadhtml.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("header").load("component/header.html",function(){
+
+  })
+  $(".banner").load("component/banner.html",function(){
+ 
+  })
+  $(".content").load("component/content.html",function(){
+    console.log(4)
+  })
+});
